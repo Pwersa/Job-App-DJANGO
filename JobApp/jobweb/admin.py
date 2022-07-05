d1a963f168d6b42e4975de5ff19edd3d2e9f31a8
sovereign31 <jrangeloindaya@gsfe.tupcavite.edu.ph>
2022-07-05 20:31:41 +0800

    MODIFY ADMIN.PY

--- JobApp/jobweb/admin.py
+++ JobApp/jobweb/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import *
 
-admin.site.register(account_registration)
-admin.site.register(job_listing)
-admin.site.register(other_info)
+class account_registration_admin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'applyingfor', 'account_type', 'date_joined']
+
+class other_info_admin(admin.ModelAdmin):
+    list_display = ['username']
+
+class job_listing_admin(admin.ModelAdmin):
+    list_display = ['jtitle', 'jdesc']
+
+
+admin.site.site_header = 'Job Web App Dashboard'
+admin.site.register(account_registration, account_registration_admin)
+admin.site.register(job_listing, job_listing_admin)
+admin.site.register(other_info, other_info_admin)
+admin.site.unregister(Group)
+
+
 
 # Register your models here.
